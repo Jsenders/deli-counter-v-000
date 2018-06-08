56a62ed371ae4768daba5afb25ffc75dbe3b367b
Joseph Senders <jasenders@gmail.com>
2018-06-08 19:34:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(x)
+line_array = []
+  if x.length == 0 
+  puts "The line is currently empty."
+end
+end
+
+def take_a_number(array, string)
+end
+
+def now_serving
+end
