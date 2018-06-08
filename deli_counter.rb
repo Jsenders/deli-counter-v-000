2cec2fac0473add36988b3e583ffe4e7d535acfd
Joseph Senders <jasenders@gmail.com>
2018-06-08 19:41:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def take_a_number(katz_deli, name)
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   end
 
-def now_serving(katz_deli)
+def now_serving(array)
   if line.empty?
     puts "There is nobody waiting to be served!"
   end
